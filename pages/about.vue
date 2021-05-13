e1a0492599d8abb1e0a7a5bc0f643a9cb9b6ab8b
erictalv <eric.talviste@tptlive.ee>
2021-05-13 17:22:55 +0300

    added responsiveness to about

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="flex flex-col min-h-screen">
-      <figure>
-        <img class="h-60 w-full object-cover" src="https://picsum.photos/670/840" alt="">
+    <div class="flex flex-col  min-h-screen md:flex-row general-padding">
+      <figure class="about-figure">
+        <img class="h-60 w-full md:h-full object-cover" src="https://picsum.photos/670/840" alt="">
       </figure>
-      <main class="about-content general-padding flex-grow  flex flex-wrap flex-col ">
+      <main class="about-content flex-grow  flex flex-wrap flex-col text-sm md:text-md xl:text-xl">
+        <button class="about-lang-btn block-inline">eng</button>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam cum ex hic odio perferendis quae quos repudiandae rerum soluta voluptatibus.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, saepe. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam consequatur cupiditate error, excepturi itaque iure magni modi nam porro praesentium quas quidem saepe sapiente, similique unde veniam voluptatem?</p>
       </main>
@@ -18,16 +19,40 @@
 
 <style lang="scss">
 
-body {
-  padding: 0;
+@media (min-width: 768px) {
+
+  .about-figure {
+    width: 100rem;
+    height: 50%;
+  }
 }
 
+
 .about-content {
+  font-family: 'Source Code Pro', monospace;
+
+  padding-right: 10%;
+  padding-left: 5%;
+
+  .about-lang-btn {
+    border-bottom: 1px solid #A5A5A5;
+    color: #A5A5A5;
+    width: fit-content;
+  }
+
   p {
     @apply
-      p-5
+      py-5
 
   }
+
+  @apply
+    pt-0
+    py-5
+
+
+
+
 }
 
 </style>
