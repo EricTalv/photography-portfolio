e8c2faf1ad9fd00749c41f27a2d3ebe4d6fc7cde
erictalv <eric.talviste@tptlive.ee>
2021-05-19 16:42:40 +0300

    about widget done

--- pages/about.vue
+++ pages/about.vue
@@ -3,17 +3,50 @@
       <figure class="about-figure">
         <img class="h-60 w-full md:h-full object-cover" src="https://picsum.photos/670/840" alt="">
       </figure>
-      <main class="about-content flex-grow  flex flex-wrap flex-col text-sm md:text-md xl:text-xl">
-        <button class="about-lang-btn block-inline">eng</button>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam cum ex hic odio perferendis quae quos repudiandae rerum soluta voluptatibus.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, saepe. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi aperiam consequatur cupiditate error, excepturi itaque iure magni modi nam porro praesentium quas quidem saepe sapiente, similique unde veniam voluptatem?</p>
+      <main class="about-content flex-grow  flex flex-wrap flex-col  text-md xl:text-xl">
+        <button @click="changeLanguage(currentLanguage)" class="about-lang-btn block-inline mt-3 md:mt-0">{{ paragraphs[currentLanguage].lang }}</button>
+        <div class="about-paragraphs w-96">
+          <div v-html="paragraphs[currentLanguage].text"></div>
+        </div>
       </main>
     </div>
 </template>
 
 <script>
     export default {
-        name: "about.vue"
+        name: "about.vue",
+
+        data() {
+          return {
+            currentLanguage: 2,
+
+            paragraphs: [
+
+              {
+                lang: "est",
+                text: "<p>This is Estonian text! <b>Hi Peeter</b></p>",
+              },
+              {
+                lang: "rus",
+                text: "<p>This is Russian text! <b>Hi Valodja</b></p>",
+              },
+              {
+                lang: "eng",
+                text: "<p>This is England! <b>Hi Shaun</b></p>",
+              }
+            ]
+          }
+        },
+
+        methods: {
+          changeLanguage(index) {
+            if (index === 2) {
+              this.currentLanguage = 0
+            } else {
+              this.currentLanguage++
+            }
+          }
+        }
     }
 </script>
 
@@ -22,11 +55,14 @@
 @media (min-width: 768px) {
 
   .about-figure {
-    width: 100rem;
-    height: 50%;
+    width: 150rem;
+    height: 30%;
   }
 }
 
+.about-paragraphs {
+  width: 37rem;
+}
 
 .about-content {
   font-family: 'Source Code Pro', monospace;
